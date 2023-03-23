ff179b417af0256a6cf8a61ab60cdd6bb52d9872
PedrocaDosCodigos <127441686+PedrocaDosCodigos@users.noreply.github.com>
2023-03-23 17:20:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const banner = document.getElementById("banner");
+
+function renderBanner() {
+banner.innerHTML = `<div> <h1>Caminhão muito doido versão deluxe 2.0</h1> <img  id="caminhao1" src="./assets/VOLVO-caminhão.jpeg"> 
+<h1> Muito rapido: Chega ate 2km/h<br/> E muito confortavel<br/> E eu ja disse que ele é rapido?</h1> </div>`
+
+   
+
+    
+    
+      
+}
+
